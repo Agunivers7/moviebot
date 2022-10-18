7c65d7f1a678fd76d0fb78a22a64eb9d1cdce3d3
Agunivers7 <herokuone7@gmail.com>
2022-10-18 11:23:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/EvaMaria.git /EvaMaria
+  git clone https://github.com/Agunivers7/moviebot.git /moviebot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /EvaMaria
+cd /moviebot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
